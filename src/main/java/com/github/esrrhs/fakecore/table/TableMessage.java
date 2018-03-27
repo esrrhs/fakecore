9fd780ec2682db8f7facb1cbcb1200273108022b
bjzhaoxin <bjzhaoxin@corp.netease.com>
2018-03-27 17:08:36 +0800

    add

--- src/main/java/com/github/esrrhs/fakecore/table/TableMessage.java
+++ src/main/java/com/github/esrrhs/fakecore/table/TableMessage.java
@@ -16,4 +16,74 @@ public final class TableMessage
 	{
 		SwitchState, Timeout, Request
 	}
+
+	public int getType()
+	{
+		return type;
+	}
+
+	public void setType(int type)
+	{
+		this.type = type;
+	}
+
+	public TablePlayerInfo getPlayerInfo()
+	{
+		return playerInfo;
+	}
+
+	public void setPlayerInfo(TablePlayerInfo playerInfo)
+	{
+		this.playerInfo = playerInfo;
+	}
+
+	public Message getMessage()
+	{
+		return message;
+	}
+
+	public void setMessage(Message message)
+	{
+		this.message = message;
+	}
+
+	public int getStateId()
+	{
+		return stateId;
+	}
+
+	public void setStateId(int stateId)
+	{
+		this.stateId = stateId;
+	}
+
+	public Object getParam()
+	{
+		return param;
+	}
+
+	public void setParam(Object param)
+	{
+		this.param = param;
+	}
+
+	public int getTimerType()
+	{
+		return timerType;
+	}
+
+	public void setTimerType(int timerType)
+	{
+		this.timerType = timerType;
+	}
+
+	public String getTimerid()
+	{
+		return timerid;
+	}
+
+	public void setTimerid(String timerid)
+	{
+		this.timerid = timerid;
+	}
 }
